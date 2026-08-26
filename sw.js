72644548ec900b65107376fd4216de95e3e8d818
AudayKid <162850257+AudayKid@users.noreply.github.com>
2026-08-26 15:37:24 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'clash-of-paw-v1';
-const FILES = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icon.svg'];
+const CACHE = 'clash-of-paw-v2';
+const FILES = ['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./whitepaper.html','./whitepaper.css','./assets/icon.svg'];
 self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES))));
 self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
 self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
